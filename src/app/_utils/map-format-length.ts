5c3dbe7a1571ade1b1a2e427a90c9d1f68e35cd8
alexandre melard <alexandre@melard.fr>
2017-10-09 12:54:00 +0200

    corrected length to display in meter up to 1km

--- src/app/_utils/map-format-length.ts
+++ src/app/_utils/map-format-length.ts
@@ -3,11 +3,11 @@ import { Sphere, geom } from 'openlayers';
 export function formatLength(line: geom.Geometry) {
     const length = Sphere.getLength(line);
     let output;
-    if (length > 100) {
+    if (length > 1000) {
         output = (Math.round(length / 1000 * 100) / 100) +
             ' ' + 'km';
     } else {
-        output = (Math.round(length * 100) / 100) +
+        output = (Math.round(length * 100 / 100)) +
             ' ' + 'm';
     }
     return output;
