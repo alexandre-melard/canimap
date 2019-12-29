54515ff5792e74013480453de021c1304c778f61
Alexandre Melard <alexandre@melard.fr>
2019-12-29 21:24:46 +0100

    updated version to 7.0.0

--- src/app/_consts/settings.ts
+++ src/app/_consts/settings.ts
@@ -23,7 +23,7 @@ export const AUTH_CONFIG: AuthConfig = {
 };
 
 export const SETTINGS = {
-    VERSION: 'v5.3.3',
+    VERSION: 'v7.0.0',
     TRACK: {
         FREQUENCY: 5,
         COLOR: '#00F'
